c49443e8713f0952be82a8a3042f0e2bfddf1856
Riley0011 <79421473+Riley0011@users.noreply.github.com>
2021-02-22 15:51:11 -0600

    Update Lessons.py

--- Lessons.py
+++ Lessons.py
@@ -29,3 +29,35 @@ abs(54 - 57)
 # (g) The lowest price among the following prices: $34.99, $29.95, and $31.50
 min(34.99, 29.95, 31.50)
 29.95
+
+
+
+# Translate the following statements into Python Boolean expressions and evaluate them:
+
+# (a) The sum of 2 and 2 is less than 4.
+2 + 2 < 4
+False
+
+# (b) The value of 7 // 3 is equal to 1 + 1.
+7 // 3 == 1 + 1
+True
+
+# (c) The sum of 3 squared and 4 squared is equal to 25.
+2 ** 3 + 4 == 25
+False
+
+# (d) The sum of 2, 4, and 6 is greater than 12.
+2 + 4 + 6 > 12
+False
+
+# (e) 1,387 is divisible by 19.
+1387 % 19 == 0
+True
+
+# (f) 31 is even. (Hint: what does the remainder when you divide by 2 tell you?)
+31 % 2 == 0
+False
+
+# (g) The lowest price among $34.99, $29.95, and $31.50 is less than $30.00.
+min(34.99, 29.95, 31.50) < 30.00
+True
