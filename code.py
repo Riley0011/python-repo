1686ffbea4b90ac420ee57156b2b5da71c38fe3c
Riley0011 <79421473+Riley0011@users.noreply.github.com>
2021-02-21 19:17:50 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,7 @@
+# This is an example we assigned a value to variable n. We then used print to print the type of value the n is. In this case the integer 3.
+n = 3
+print(type(n))
+
 # This for loop with print the highest number from the list of numbers.
 numbers = [3, 6, 2, 8, 4, 10]
 max = numbers[0]
