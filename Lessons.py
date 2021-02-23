eb2da6d3965e28b357969221cd530ef3e430621a
Riley0011 <79421473+Riley0011@users.noreply.github.com>
2021-02-23 12:48:48 -0600

    Update Lessons.py

--- Lessons.py
+++ Lessons.py
@@ -142,7 +142,7 @@ s[9]
 
 
 # First execute the assignment
-# words = ['bat', 'ball', 'barn', 'basket', 'badminton']
+words = ['bat', 'ball', 'barn', 'basket', 'badminton']
 # Now write two Python expressions that evaluate to the first and last, respectively, word in words, in dictionary order.
 min(words)
 'badminton'
