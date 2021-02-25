06837c1199497589e4dd61fd655608d5f07a70a3
Riley0011 <79421473+Riley0011@users.noreply.github.com>
2021-02-25 15:17:00 -0600

    Update Lessons.py

--- Lessons.py
+++ Lessons.py
@@ -177,20 +177,20 @@ sum(grades) / len(grades)
 
 
 
-In what order are the operators in the below expressions evaluated?
-(a) 2 + 3 == 4 or a >= 5
+# In what order are the operators in the below expressions evaluated?
+# (a) 2 + 3 == 4 or a >= 5
 ((2 + 3) == 4) or (a >= 5)
 
-(b) lst[1] * -3 < -10 == 0
+# (b) lst[1] * -3 < -10 == 0
 (((lst[1]) * (-3)) < (-10)) == 0
 
-(c) (lst[1] * -3 < -10) in [0, True]
+# (c) (lst[1] * -3 < -10) in [0, True]
 (((lst[1]) * (-3)) < (-10)) in [0, True]
 
-(d) 2 * 3**2
+# (d) 2 * 3**2
 2 * (3**2)
 
-(e) 4 / 2 in [1, 2, 3]
+# (e) 4 / 2 in [1, 2, 3]
 (4 / 2) in [1, 2, 3]
 
 
